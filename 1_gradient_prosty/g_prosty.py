7512f9b9b2e6b0048e41ec5c5c5a1ded833a7376
Anna Sosulska <anna.sosulska.stud@pw.edu.pl>
2021-10-17 15:20:33 +0200

    bugfixes & 2nd function added

--- 1_gradient_prosty/g_prosty.py
+++ 1_gradient_prosty/g_prosty.py
@@ -1,39 +1,54 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import random
 
 
-def y(x):
-    return 17.8 * (x - 20) ** 2 + 0.3 * x + 43.6
+def y1(x):
+    return 3.04 * x ** 2 + 3.76 * x - 18
 
 
-def gradient(x1, x2):
+def y2(x):
+    return 0.0034 * x  ** 4  - 20 * x ** 2 - 170 * x
+
+
+def gradient(y, x1, x2):
     if (x2 - x1) != 0:
         return (y(x2) - y(x1)) / (x2 - x1)
     else:
         return 1
 
 
-plot_range = [0, 100]
-x = np.arange(-100, 100)
-plt.plot(x, y(x))
+def gradient_descent(x, y, h, x0, func: str):
+    plt.figure()
+    x2_array = []
+    y2_array = []
+    plt.plot(x, y(x))
+    plt.plot(x0, y(x0), 'r.')
+    xi1 = x0
+    xi2 = xi1 + h
+    epsilon = 0.5
+    while abs(gradient(y, xi1, xi2)) >= epsilon:
+        x2_array.append(xi2)
+        y2_array.append(y(xi2))
+
+        xi1, xi2 = xi2, xi2 - h * gradient(y, xi1, xi2)
+        #print("xi1", xi1)
+        #print("xi2", xi2)
+        print("gradient(y, xi1, xi2)", gradient(y, xi1, xi2))
+    plt.plot(x2_array, y2_array, 'g.--')
+    for i in range(len(x2_array)):
+        plt.annotate(str(i), xy=(x2_array[i], y2_array[i]+y(x[0])/20), fontsize=6)
+    plt.annotate(f"h = {h}", xy=(x[0], 0))
+    ann2 = "$x_{min}$ =" + "{my_min:.2f}".format(my_min=xi2)
+    plt.annotate(ann2, xy=(x[0], y(x[0]) / 10))
+    plt.annotate(f"$\epsilon = {epsilon}$", xy=(x[0], y(x[0])* 2 / 10))
+    plt.title(func)
+    plt.show()
 
 
-h = 0.5 # dlugość kroku
-x0 = random.randrange(plot_range[0], plot_range[1])     # punkt startowy
-
-plt.plot(x0, y(x0), 'r.')
-xi1 = x0
-xi2 = xi1 + h
-epsilon = 0.5
-while np.abs(gradient(xi1, xi2)) >= epsilon:
-    print(xi2)
-    if y(xi2) > y(xi1):
-        h = h / 2
-        xi2 = xi1 - h * gradient(xi1, xi2)
-    if y(xi2) < y(xi1):
-        xi1 = xi1 - h * gradient(xi1, xi2)
-        xi2 = xi2 - h * gradient(xi1, xi2)
-print(xi2)
-plt.plot(xi2, y(xi2), 'go')
-plt.show()
+if __name__ == "__main__":
+    h = 2*1e-2  # dlugość kroku
+    x = np.arange(-100, 100)
+    #gradient_descent(x, y1, h, 70, "$3.04 \cdot x^2 + 3.76 \cdot x - 18$")
+    gradient_descent(x, y2, h, -100, "$0.0034 \cdot x  ** 4  - 20 \cdot x ** 2 - 170 \cdot x$")
+    """plt.plot(x, y2(x))
+    plt.show()"""
