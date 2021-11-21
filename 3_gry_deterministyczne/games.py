087309db0dba60515167bd10f18ab7bca115a19a
Anna Sosulska <anna.sosulska.stud@pw.edu.pl>
2021-11-22 00:23:46 +0100

    fix alpha_beta function, added function for making measurements

--- 3_gry_deterministyczne/games.py
+++ 3_gry_deterministyczne/games.py
@@ -1,4 +1,5 @@
 import pytest
+from random import choice
 
 """Min is using O, while Max uses X"""
 
@@ -64,7 +65,7 @@ class Board:
                     return False
         return True
 
-    def is_terminal(self, player):
+    def player_won(self, player):
         # check columns
         for i in range(len(self.board)):
             terminal = True
@@ -99,17 +100,25 @@ class Board:
                 break
         if terminal:
             return terminal
+
+    def is_terminal(self, player):
+        if self.player_won(player):
+            return True
         # check if full
         return self.is_board_full()
 
-    def successors(self, player):
-
-        # create a list of available fields
+    def list_available(self):
         available = []
         for i in range(len(self.board)):
             for j in range(len(self.board)):
                 if self.board[i][j].sign == "":
                     available.append(i * 3 + j + 1)
+        return available
+
+    def successors(self, player):
+
+        # create a list of available fields
+        available = self.list_available()
         # generate successors
         successors = []
 
@@ -120,36 +129,44 @@ class Board:
         return successors
 
 
-def alpha_beta(state, depth, max_move, alpha, beta):
-    if state.is_terminal() or depth == 0:
+def alpha_beta(state, depth, player, alpha, beta):
+    if state.is_terminal(player) or depth == 0:
         return state.heuristics()
-    U = state.successors()
-    if max_move:
+    U = state.successors(player)
+    if player == 1:
         for u in U:
-            alpha = max(alpha, alpha_beta(u, depth - 1, alpha, beta))
-            if alpha >> beta:
-                return beta
+            alpha = max((alpha, alpha_beta(Board(u), depth - 1, -player, alpha, beta)))
+            if alpha <= beta:
+                return alpha
         return alpha
+    else:
+        for u in U:
+            beta = min((beta, alpha_beta(Board(u), depth - 1, -player, alpha, beta)))
+            if alpha <= beta:
+                return beta
+        return beta
     pass
 
 
-def min_max(state: Board, depth, player):
+def min_max(state: Board, depth, player, a=None, b=None):
     if state.is_terminal(player) or depth == 0:
         return state.heuristics()
     U = state.successors(player)
     score = []
     for u in U:
-        score.append(min_max(state, depth - 1, -player))
-    #print(player, ":\t", score)
+        score.append(min_max(Board(u), depth - 1, -player))
     if player == 1:
         return max(score)
     if player == -1:
         return min(score)
 
 
-def next_move(state: Board, depth, player):
+def next_move(state: Board, depth=0, player=1, algorithm=min_max):
+    if algorithm == rand_move:
+        return rand_move(state)
+
     if player == 1:
-        best_score = 0
+        best_score = -20
     else:
         best_score = 20
     best_move = (None, None)
@@ -158,7 +175,7 @@ def next_move(state: Board, depth, player):
             if state.board[i][j].sign == "":
                 state_dict = state.board_state()
                 state_dict[i * 3 + j + 1] = player
-                score = min_max(Board(state_dict), depth, player)
+                score = algorithm(Board(state_dict), depth, player, -100, 100)
                 if player == 1:
                     if score > best_score:
                         best_score = score
@@ -167,18 +184,55 @@ def next_move(state: Board, depth, player):
                     if score < best_score:
                         best_score = score
                         best_move = (i, j)
-    print(best_move)
+    #print(player, ":\t", best_move)
     return best_move
 
-def main():
-    player = 1
-    my_board = Board()
-    while not my_board.is_terminal(player):
-        x, y = next_move(my_board, 2, player)
-        my_board.change_state(x, y, player)
-        my_board.print_board()
-        player *= -1
 
+def rand_move(state: Board):
+    available = state.list_available()
+    move = choice(available)
+    return (move - 1) // 3, (move - 1) % 3
+
+
+def make_stats(algorithm1, algorithm2, trials, depth1=0, depth2=0):
+    who_won = {-1: 0, 1: 0, "Draw": 0}
+    for _ in range(trials):
+        player = 1
+        my_board = Board()
+        won_by_player = False
+        while not my_board.is_board_full():
+            if player == 1:
+                x, y = next_move(my_board, depth1, player, algorithm1)
+            elif player == -1:
+                x, y = next_move(my_board, depth2, player, algorithm2)
+            my_board.change_state(x, y, player)
+            #my_board.print_board()
+            if my_board.player_won(player):
+                won_by_player = True
+                who_won[player] += 1
+                break
+            player *= -1
+        if not won_by_player:
+            who_won["Draw"] += 1
+    dict_for_print = {min_max: "Algorytm Minimax", alpha_beta: "Algorytm Minimax \u03B1 - \u03B2",
+                      rand_move: "AI losujący swój ruch", -1: "Gracz Min",
+                      1: "Gracz Max", "Draw": "Remis"}
+    print("Liczba prób:\t", trials, "\nGracz Max:\n\tZastosowany algorytm:\t", dict_for_print[algorithm1], "\n\tGłębokość:\t", depth1,
+          "\nGracz Min:\n\tZastosowany algorytm:\t", dict_for_print[algorithm2], "\n\tGłębokość:\t", depth2,
+          "\nWyniki:\n", "\tGracz Max", who_won[1], "\n\tGracz Min", who_won[-1], "\n\tRemis", who_won["Draw"])
+
+
+def main():
+    make_stats(min_max, rand_move, 15, depth1=4)
+    make_stats(min_max, min_max, 7, depth1=9, depth2=9)
+    make_stats(min_max, alpha_beta, 7, depth1=9, depth2=9)
+    make_stats(min_max, min_max, 7, depth1=9, depth2=3)
+    make_stats(min_max, alpha_beta, 7, depth1=3, depth2=9)
+    make_stats(min_max, min_max, 7, depth1=3, depth2=3)
+    make_stats(min_max, alpha_beta, 7, depth1=3, depth2=3)
+    make_stats(min_max, min_max, 7, depth1=1, depth2=0)
+    make_stats(min_max, min_max, 7, depth1=0, depth2=9)
+    make_stats(rand_move, rand_move, 20)
 
 if __name__ == "__main__":
     main()
