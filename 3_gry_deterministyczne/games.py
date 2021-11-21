25f22af6122cd0d9708837f9e7a05b99e630bd71
Anna Sosulska <anna.sosulska.stud@pw.edu.pl>
2021-11-21 17:57:39 +0100

    change indexing, implement is_terminal method + more tests

--- 3_gry_deterministyczne/games.py
+++ 3_gry_deterministyczne/games.py
@@ -1,8 +1,11 @@
 import pytest
+
 """Min is using O, while Max uses X"""
 
 
 class Field:
+
+
     def __init__(self, value, sign=""):
         self.value = value
         self.sign = sign
@@ -16,44 +19,86 @@ class Field:
 
 class Board:
     def __init__(self, states=None):
-        self.board = {}
+        self.board = []
         self.state = 0
-        values = [3, 2, 3, 2, 4, 2, 3, 2, 3]
-        for i in range(9):
-            self.board[i+1] = Field(values[i])
+        values = [[3, 2, 3], [2, 4, 2], [3, 2, 3]]
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append(Field(values[i][j]))
+            self.board.append(row)
         if states:
             for s_key, s_val in states.items():
-                self.board[s_key].fill_field(s_val)
+                self.board[(s_key - 1) // 3][(s_key - 1) % 3].fill_field(s_val)
 
     def heuristics(self):
         state = 0
-        for i in range(9):
-            if self.board[i + 1].sign != "":
-                state += self.board[i+1].value
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j].sign != "":
+                    state += self.board[i][j].value
         self.state = state
         return state
 
-    def change_state(self, idx, sign):
-        if idx in range(1, 10) and sign in ["O", "X"]:
-            self.board[idx].sign = sign
+    def change_state(self, idx, idy, sign):
+        if idx in range(3) and idx in range(3) and sign in ["O", "X"]:
+            self.board[idx][idy].sign = sign
 
 
-def is_terminal(s):
-    pass
+    def is_board_full(self):
+        for row in self.board:
+            for item in row:
+                if item.sign == "":
+                    return False
+        return True
 
+    def is_terminal(self, player):
+        # check columns
+        for i in range(len(self.board)):
+            terminal = True
+            for j in range(len(self.board)):
+                if self.board[j][i].sign is not player:
+                    terminal = False
+                    break
+            if terminal:
+                return terminal
+        # check rows
+        for i in range(len(self.board)):
+            terminal = True
+            for j in range(len(self.board)):
+                if self.board[i][j].sign is not player:
+                    terminal = False
+                    break
+            if terminal:
+                return terminal
+        # check diagonals
+        terminal = True
+        for i in range(len(self.board)):
+            if self.board[i][i].sign is not player:
+                terminal = False
+                break
+        if terminal:
+            return terminal
+        # 0,2 1,1, 2,0
+        terminal = True
+        for i in range(len(self.board)):
+            if self.board[i][2-i].sign is not player:
+                terminal = False
+                break
+        if terminal:
+            return terminal
+        # check if full
+        return self.is_board_full()
 
-def heuristics(s):
-    pass
+    def successors(self):
 
-
-def successors(s):
-    return []
+        pass
 
 
 def alphabeta(state, depth, max_move, alpha, beta):
-    if is_terminal(state) or depth == 0:
-        return heuristics(state)
-    U = successors(state)
+    if state.is_terminal() or depth == 0:
+        return state.heuristics()
+    U = state.successors()
     if max_move:
         for u in U:
             alpha = max(alpha, alphabeta(u, depth - 1, alpha, beta))
@@ -80,10 +125,25 @@ def test_empty_board():
 
 def test_changing_state():
     my_board = Board()
-    my_board.change_state(5, "O")
+    my_board.change_state(1, 1, "O")
     assert my_board.heuristics() == 4
 
 
 def test_filled_board():
     my_board = Board({3: "X", 5: "O", 9: "X"})
     assert my_board.heuristics() == 2
+
+def test_is_full():
+    pass
+
+@pytest.mark.parametrize('state, player, is_terminal',
+                         [
+                             ({1: "O", 2: "O", 5: "O", 9: "X"}, "X", False),
+                             ({1: "O", 5: "O", 7: "X", 9: "X"}, "X", False),
+                             ({1: "O", 5: "O", 6: "X", 9: "X"}, "X", False),
+                             ({1: "X", 4: "X", 7: "X"}, "X", True),     # first column
+                             ({1: "X", 5: "X", 9: "X"}, "X", True),     # diagonal
+                             ({1: "X", 3: "O", 4: "X", 5: "O", 7: "O", 9: "X"}, "O", True)      # diagonal O
+                         ])
+def test_terminal_states(state: dict, player, is_terminal):
+    assert Board(state).is_terminal(player) == is_terminal
